92e87c611ea15e92a0a0174a17402c483b3f3abd
Drew King <andrew.phillip.king@gmail.com>
2024-12-15 10:26:32 -0800

    Got the layout down.

--- src/app/components/ProjectImagesBlock.tsx
+++ src/app/components/ProjectImagesBlock.tsx
@@ -15,13 +15,11 @@ interface ProjectImagesBlockProps {
 
 const ProjectImagesBlock: React.FC<ProjectImagesBlockProps> = ({ images, direction = 'row', width, maxHeight }) => {
   return (
-    <div className={`project-images-block flex ${direction === 'row' ? 'flex-col' : 'flex-row'}`} style={{ maxHeight }}>
+    <div className={`flex ${direction === 'row' ? 'flex-row' : 'flex-col'} `} style={{ maxHeight }}>
       {images.map((image, index) => (
-        <div key={index} className={`flex ${direction === 'row' ? 'flex-col' : 'flex-row'} gap-2`} style={{ maxWidth: `${width * 100/ images.length + 50}px` }}>
-          <div className="image-container block">
-            <Image src={image.src} alt={image.alt} width={width * 100} height={400} className="image" />
-            <p className={`inline w-${width * 100} overflow-none whitespace`}>{image.caption}</p>
-          </div>
+        <div key={index} className={`image-container flex flex-col`} style={{ maxWidth: `${width * 100+ 50 }px` }}>
+          <Image src={image.src} alt={image.alt} width={width * 100} height={400} className="image" />
+          <p className={`inline w-${width * 100} overflow-none whitespace italic small mb-2`}>{image.caption}</p>
         </div>
       ))}
     </div>
