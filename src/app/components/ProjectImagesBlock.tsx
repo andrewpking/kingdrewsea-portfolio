c75fe3457791a86d3c2b0a7bbd87748fd25f98ef
Drew King <andrew.phillip.king@gmail.com>
2024-12-15 11:07:06 -0800

    Added children to ImagesBlock

--- src/app/components/ProjectImagesBlock.tsx
+++ src/app/components/ProjectImagesBlock.tsx
@@ -13,15 +13,20 @@ interface ProjectImagesBlockProps {
   maxHeight: string;
 }
 
-const ProjectImagesBlock: React.FC<ProjectImagesBlockProps> = ({ images, direction = 'row', width, maxHeight }) => {
+const ProjectImagesBlock: React.FC<ProjectImagesBlockProps> = ({ images, direction = 'row', width, maxHeight, children }) => {
   return (
-    <div className={`flex ${direction === 'row' ? 'flex-row' : 'flex-col'} `} style={{ maxHeight }}>
-      {images.map((image, index) => (
-        <div key={index} className={`image-container flex flex-col`} style={{ maxWidth: `${width * 100+ 50 }px` }}>
-          <Image src={image.src} alt={image.alt} width={width * 100} height={400} className="image" />
-          <p className={`inline w-${width * 100} overflow-none whitespace italic small mb-2`}>{image.caption}</p>
-        </div>
-      ))}
+    <div className="flex flex-row">
+      <section className={`inline-flex ${direction === 'row' ? 'flex-row' : 'flex-col'} mb-4 `} style={{maxHeight: `${maxHeight} px`, maxWidth: `${width * 100 * images.length + 50} px`}}>
+        {images.map((image, index) => (
+          <div key={index} className={`image-container flex flex-col`} style={{ maxWidth: `${width * 100+ 50 }px` }}>
+            <Image src={image.src} alt={image.alt} width={width * 100} height={400} className="image" />
+            <p className={`inline w-${width * 100} overflow-none whitespace italic small mb-2`}>{image.caption}</p>
+          </div>
+        ))}
+      </section>
+      <div className="children-container inline float-right">
+        {children}
+      </div>
     </div>
   );
 };
