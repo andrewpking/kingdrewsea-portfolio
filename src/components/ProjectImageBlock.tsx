af6935fecc441b799c68f4c33fd8e45b58f42689
Drew King <andrew.phillip.king@gmail.com>
2024-12-15 07:26:40 -0800

    Created componenets that MindMii depends on.

---
+++ src/components/ProjectImageBlock.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface Image {
+  src: string;
+  alt: string;
+  caption: string;
+}
+
+interface ProjectImagesBlockProps {
+  images: Image[];
+  direction?: 'row' | 'column';
+  width: number;
+  maxHeight: string;
+}
+
+const ProjectImagesBlock: React.FC<ProjectImagesBlockProps> = ({ images, direction = 'row', width, maxHeight }) => {
+  return (
+    <div className={`project-images-block flex flex-${direction} gap-4`} style={{ maxWidth: `${width * 100}px`, maxHeight }}>
+      {images.map((image, index) => (
+        <div key={index} className="image-container">
+          <Image src={image.src} alt={image.alt} className="image" />
+          <p className="caption">{image.caption}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProjectImagesBlock;
---
+++ src/components/ProjectTeamGallery.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface Member {
+  name: string[];
+  photo: string;
+}
+
+interface ProjectTeamGalleryProps {
+  members: Member[];
+}
+
+const ProjectTeamGallery: React.FC<ProjectTeamGalleryProps> = ({ members }) => {
+  return (
+    <div className="team-gallery">
+      {members.map((member, index) => (
+        <div key={index} className="team-member">
+          <Image
+            src={member.photo}
+            alt={`Photo of ${member.name[0]}.`}
+            className="team-member-photo"
+          />
+          <div className="team-member-info">
+            <h3>{member.name[0]}</h3>
+            <p>{member.name[1]}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProjectTeamGallery;
