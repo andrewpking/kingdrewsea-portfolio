3dbbb7b5c2a8dc85cb3ac43e88dcc3a9b05c09ca
Drew King <andrew.phillip.king@gmail.com>
2024-12-16 09:47:56 -0800

    Removed z-order from the returned HTML.

--- src/app/components/MdxPage.tsx
+++ src/app/components/MdxPage.tsx
@@ -35,10 +35,10 @@ const MdxPage: React.FC<MdxPageProps> = ({ metadata, children }) => {
                     />
                     <div className="absolute inset-0 overlay"></div>
                     <div className="absolute inset-0 flex flex-col items-center justify-center p-4">
-                        <h1 className="text-4xl font-bold z-10">
+                        <h1 className="text-4xl font-bold">
                             {metadata.title}
                         </h1>
-                        <p className="text-white z-10 text-center">
+                        <p className="text-white text-center">
                             {metadata.description}
                         </p>
                     </div>
