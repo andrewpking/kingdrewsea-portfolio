38b8ba003391cece567632b51cc4adfa3063d138
Drew King <andrew.phillip.king@gmail.com>
2024-12-18 17:03:22 -0800

    Renamed metadata to meta to avoid mixups with next metadata type.

--- src/app/components/MdxPage.tsx
+++ src/app/components/MdxPage.tsx
@@ -9,15 +9,15 @@ interface MdxPageProps {
     children?: React.ReactNode;
 }
 
-const MdxPage: React.FC<MdxPageProps> = ({ meta: metadata, children }) => {
+const MdxPage: React.FC<MdxPageProps> = ({ meta, children }) => {
     return (
         <>
             <Header />
             <main className="project-page">
                 <div className="image-head-container relative md:mt-20 md:pt-4 sm:mt-12 sm:pt-0">
                     <Image 
-                        src={metadata.image} 
-                        alt={metadata.imageAlt} 
+                        src={meta.image} 
+                        alt={meta.imageAlt} 
                         width={1120} 
                         height={150}
                         className="w-full h-[250px] md:h-[200px] object-cover"
@@ -26,17 +26,17 @@ const MdxPage: React.FC<MdxPageProps> = ({ meta: metadata, children }) => {
                     <div className="absolute inset-0 overlay"></div>
                     <div className="absolute inset-0 flex flex-col items-center justify-center p-4">
                         <h1 className="text-4xl font-bold">
-                            {metadata.title}
+                            {meta.title}
                         </h1>
                         <p className="hidden sm:block text-center">
-                            {metadata.description}
+                            {meta.description}
                         </p>
                     </div>
                 </div>
 
                 {/* Visible only on screens smaller than md */}
                 <p className="sm:hidden text-center px-4 py-6">
-                    {metadata.description}
+                    {meta.description}
                 </p>
                 {children}
             </main>
