96365243beed3cd5a223964377b5a33fc5e48ce8
Drew King <andrew.phillip.king@gmail.com>
2025-06-16 08:25:51 -0700

    Added links to about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,22 +15,33 @@ export default function Home() {
             technologists, this shows in their course projects from the Allen
             School at UW and current work at KBCS on a cross functional team.
             They are a published researcher in the field of Data Science, as
-            coauthor of the paper, &quot;Practical considerations for variable
-            screening in the super learner&quot;, in the The New England Journal
-            of Statistics in Data Science. In Drew&apos;s free time they are an
-            avid reader, world traveler, and enjoy baking gluten free pastries.
-            They live in the Seattle area with their house plants.
+            coauthor of the paper, &quot;
+            <a href="https://doi.org/10.51387/25-NEJSDS82">
+              Practical considerations for variable screening in the super
+              learner
+            </a>
+            &quot;, in the{" "}
+            <a href="https://nejsds.nestat.org/journal/NEJSDS">
+              The New England Journal of Statistics in Data Science
+            </a>
+            . In Drew&apos;s free time they are an avid reader, world traveler,
+            and enjoy baking gluten free pastries. They live in the Seattle area
+            with their house plants.
           </p>
           <p>
             As a firm believer in accessibility, Drew was the Co-Chair of a
             student organization whose mission was to foster a culture of
             inclusion for people with disabilities in the Allen School and
             educate students about creating accessible software. As Co-Chair of
-            Ability, Drew educated the team about accessible document creation
-            and started a book club. Drew collaborated with a peer, Lee
-            Janzen-Morel, on an initiative called &quot;Multicultural
-            Matrix&quot;, which was influential in the creation of a Diversity
-            and Access Lounge on the Allen School Campus for underrepresented
+            <a href="https://ability.cs.washington.edu/">Ability</a>, Drew
+            educated the team about accessible document creation and started a
+            book club. Drew collaborated with a peer, Lee Janzen-Morel, on an
+            initiative called &quot;
+            <a href="https://multiculturalmatrix.github.io/">
+              Multicultural Matrix
+            </a>
+            &quot;, which was influential in the creation of a Diversity and
+            Access Lounge on the Allen School Campus for underrepresented
             students. In the future, Drew would like to continue working on a
             cross functional team building interactive experiences that are user
             friendly to diverse audiences and creating inclusive communities.
@@ -38,10 +49,17 @@ export default function Home() {
           <p>
             Community service and stewardship are values that Drew cares deeply
             about. Although Drew does not use much social media anymore, they
-            are an administrator for a bluesky account for the organization
-            &quot;Disabled In Computing&quot; and created a website for them on
-            Google Sites. Drew also runs a personal bluesky account where they
-            talk about their experiences as a technologist.
+            are an administrator for a{" "}
+            <a href="https://disabledincs.bsky.social">
+              bluesky account for the organization &quot;Disabled In
+              Computing&quot;
+            </a>{" "}
+            and created a{" "}
+            <a href="https://sites.google.com/view/disabledincs">
+              website for them on Google Sites
+            </a>
+            . Drew also runs a personal bluesky account where they talk about
+            their experiences as a technologist.
           </p>
         </main>
         <aside className="mt-8 lg:mt-0">
