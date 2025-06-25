1ee32fefe57ba8ddc4128bc2c4e6b48d9f15e42d
Drew King <andrew.phillip.king@gmail.com>
2025-06-25 11:25:13 -0700

    Added OpenGraph interface.

--- src/app/components/Metadata.ts
+++ src/app/components/Metadata.ts
@@ -1,9 +1,23 @@
+interface OpenGraphMetadata {
+  type: "website" | "article" | "profile";
+  locale?: string;
+  siteName?: string;
+  images?: {
+    url: string;
+    width?: number;
+    height?: number;
+    alt: string;
+  }[];
+}
+
 export default interface PageMetadata {
-    title: string;
-    description: string;
-    image: string;
-    imageAlt: string;
-    href: string;
-    date: string;
-    authors: string;
+  // Basic metadata
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  href: string;
+  date: string;
+  authors: string;
+  openGraph?: OpenGraphMetadata;
 }
