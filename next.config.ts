ba0510b08d327cbdeb7880284d3d37732a91cae3
Drew King <andrew.phillip.king@gmail.com>
2025-06-25 10:07:44 -0700

    Added redirects from old project page paths to new paths. This will reduce 404 errors.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,45 @@
-import createMDX from '@next/mdx'
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import createMDX from "@next/mdx";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
- 
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
+  // Old links to projects now redirect to the projects page.
+  async redirects() {
+    return [
+      {
+        source: "/kbcs",
+        destination: "/projects/kbcs",
+        permanent: true,
+      },
+      {
+        source: "/high-speed-rail",
+        destination: "/projects/high-speed-rail",
+        permanent: true,
+      },
+      {
+        source: "/mindmii",
+        destination: "/projects/mindmii",
+        permanent: true,
+      },
+      {
+        source: "/resitogether",
+        destination: "/projects/resitogether",
+        permanent: true,
+      },
+      {
+        source: "/sl-screens",
+        destination: "/projects/sl-screens",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
- 
+});
+
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
