0cb73ff920872ad180c085841abd262dd8b1d244
Drew King <andrew.phillip.king@gmail.com>
2025-06-25 11:25:26 -0700

    Added support for openGraph for link previews.

--- src/app/components/pages.ts
+++ src/app/components/pages.ts
@@ -1,5 +1,10 @@
 import PageMetadata from "./Metadata";
 
+const DEFAULT_OG = {
+  locale: "en_US",
+  siteName: "Drew King - Portfolio",
+};
+
 /** Create an object that contains a map of all pages **/
 /** The URL should be the key **/
 /** The value should be a Record object **/
@@ -8,38 +13,86 @@ const pages: Record<string, PageMetadata> = {
     title: "Home",
     description:
       "My name is Drew and I am an aspring software engineer with a degree in Computer Science from the Paul G Allen School. I am passionate about creating software that is both beautiful and accessible. I have experience with a variety of technologies and am always looking to learn more. I am currently looking for a full-time software engineering role.",
-    image: "",
-    imageAlt: "",
+    image: "/images/og-home.png", // Add a default home page image
+    imageAlt: "Drew King's Portfolio Homepage",
     href: "/",
     date: "2024-12-15",
     authors: "Drew King",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "website",
+      images: [
+        {
+          url: "/images/og-home.png",
+          width: 1200,
+          height: 630,
+          alt: "Drew King's Portfolio Homepage",
+        },
+      ],
+    },
   },
   "/about": {
     title: "About",
     description: "Learn more about me",
-    image: "",
-    imageAlt: "",
+    image: "/images/og-about.png", // Add an about page image
+    imageAlt: "Drew King - About Page",
     href: "/about",
     date: "2025-06-15",
     authors: "Drew King",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "profile",
+      images: [
+        {
+          url: "/images/og-about.png",
+          width: 1200,
+          height: 630,
+          alt: "Drew King - About Page",
+        },
+      ],
+    },
   },
   "/projects": {
     title: "Projects",
     description: "A collection of my projects",
-    image: "",
-    imageAlt: "",
+    image: "/images/og-projects.png", // Add a projects page image
+    imageAlt: "Drew King's Project Portfolio",
     href: "/projects",
     date: "2025-06-15",
     authors: "Drew King",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "website",
+      images: [
+        {
+          url: "/images/og-projects.png",
+          width: 1200,
+          height: 630,
+          alt: "Drew King's Project Portfolio",
+        },
+      ],
+    },
   },
   "/Drew_King-cv.pdf": {
     title: "CV",
     description: "My CV",
-    image: "",
-    imageAlt: "",
+    image: "/images/og-cv.png", // Add a CV preview image
+    imageAlt: "Drew King's CV",
     href: "/Drew_King-CV.pdf",
     date: "2025-06-15",
     authors: "Drew King",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/og-cv.png",
+          width: 1200,
+          height: 630,
+          alt: "Drew King's CV",
+        },
+      ],
+    },
   },
 };
 
@@ -53,6 +106,18 @@ const projects: Record<string, PageMetadata> = {
     href: "/projects/high-speed-rail",
     date: "2024-12-15",
     authors: "Drew King, Joey Krueger, Jack Rosenbloom",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/high-speed-rail-cover.png",
+          width: 1200,
+          height: 630,
+          alt: "A screenshot of the High Speed Rail project, showing a map of the United States with a train route drawn on it.",
+        },
+      ],
+    },
   },
   kbcs: {
     title: "KBCS Theme",
@@ -63,6 +128,18 @@ const projects: Record<string, PageMetadata> = {
     href: "/projects/kbcs",
     date: "2024-12-15",
     authors: "Drew King, Sama Khalid, Colin Xiao, Sophia Lin",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/KBCS_Menu-Bar_Flow.png",
+          width: 1200,
+          height: 630,
+          alt: "KBCS Theme Project Overview",
+        },
+      ],
+    },
   },
   MindMii: {
     title: "MindMii",
@@ -73,6 +150,18 @@ const projects: Record<string, PageMetadata> = {
     href: "/projects/mindmii",
     date: "2024-03-07",
     authors: "Drew King, Lee Janzen, Christoph Bendix",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/mindmii/MindMii_Cover.png",
+          width: 1200,
+          height: 630,
+          alt: "MindMii Project Overview",
+        },
+      ],
+    },
   },
   ResiTogether: {
     title: "ResiTogether",
@@ -85,6 +174,18 @@ const projects: Record<string, PageMetadata> = {
     date: "2023-06-05",
     authors:
       "Drew King, Weather Nguyen, Taylor Richmond, Julie McUne, Jessica Wang",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/resitogether/ResiTogether_cover.png",
+          width: 1200,
+          height: 630,
+          alt: "ResiTogether Project Overview",
+        },
+      ],
+    },
   },
   SLScreens: {
     title: "SL Screens",
@@ -94,6 +195,18 @@ const projects: Record<string, PageMetadata> = {
     href: "/projects/sl-screens",
     date: "2021-08-08",
     authors: "Drew King, Brian D Williamson, Ying Huang",
+    openGraph: {
+      ...DEFAULT_OG,
+      type: "article",
+      images: [
+        {
+          url: "/images/lasso_vs_sl.png",
+          width: 1200,
+          height: 630,
+          alt: "SL Screens Project Overview",
+        },
+      ],
+    },
   },
 };
 
