0cae2b871f9fa02f0437b480087448b1da8c1438
Drew King <andrew.phillip.king@gmail.com>
2025-07-30 15:15:44 -0700

    Use packages for preventing XSS on bluesky.

--- src/app/components/BlueSkyTimeline.tsx
+++ src/app/components/BlueSkyTimeline.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import { JSDOM } from "jsdom";
+import DOMPurify from "dompurify";
 
 interface BlueskyTimelineProps {
   feedUrl: string;
@@ -44,7 +46,14 @@ const BlueskyTimeline: React.FC<BlueskyTimelineProps> = ({
 
       try {
         const html = await getHtml(urlWithTimestamp);
-        containerRef.current.innerHTML = html;
+        const cfg = {
+          ALLOW_TAGS: ["div", "p", "a", "img", "video"],
+          FORBID_TAGS: ["script", "iframe", "style"],
+          FORBID_ATTR: ["style"],
+          ALLOW_DATA_ATTR: false,
+        };
+        const sanitizedHTML = DOMPurify.sanitize(html, cfg);
+        containerRef.current.innerHTML = sanitizedHTML;
       } catch (error) {
         console.error("Error fetching Bluesky timeline:", error);
         if (containerRef.current) {
